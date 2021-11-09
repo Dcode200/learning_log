69990a38d91091cdd8437f04d0fa607e99efaafe
Dcode200 <dcode_linux@protonmail.com>
2021-11-09 20:06:45 +0100

    Added new_entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The homepage for learning log."""
@@ -10,3 +11,27 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """add a new topic."""
+    if request.method != 'POST':
+        #No data submitted:
+        form = TopicForm()
+    else:
+        #POST data submitted
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
